297b985194b9b0eb8bbef212102444cc0a2add7e
datdoc <uncledrew2youngblood@gmail.com>
2021-04-25 10:04:22 +0700

    add inference.py

--- src/inference.py
+++ src/inference.py
@@ -2,6 +2,7 @@ import argparse
 from tqdm import tqdm
 import pandas as pd
 import numpy as np
+import os
 
 import torch 
 
@@ -22,55 +23,69 @@ def inference_one_epoch(model, data_loader, device):
     return image_preds_all
 
 
+def load_models(models_path, device):
+    model_names = os.listdir(models_path)
+    models = []
+    n_class = 13
+    for path in models_names:
+        
+        if "efficientnet" in path:
+            model = Classifier("tf_efficientnet_b4_ns", n_classes).to(device)
+        elif "vit" in path:
+            model = Classifier("vit_base_patch16_384", n_classes).to(device)
+        elif "resnext" in path:
+            model = Classifier("resnext50_32x4d", n_classes).to(device)
+
+        for idx in range(5): # number of fold
+            model_path = os.path.join(path, "fold{}".format(idx))
+
+            model.load_state_dict(torch.load(os.path.join(model_path, "best.pt"))['model'])
+
+            models.append(model)
+            del model
+
+    return models
+
+
 def run_infer(opt):
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
     arch_weights = np.ones(15) # set weight for each arch base on f1 score on CV
-    model_names = ['tf_efficientnet_b4_ns','resnext50_32x4d','vit_base_patch16_384']
+   
     n_classes = 13
     weight_path = opt.weight_path
-    weights =  sorted(os.listdir(weight_path))
+    tst_preds = []
+
     
-    for ix, model_arch in enumerate(model_names):
 
-        TEST_DIR = opt.test_dir
-        test = pd.DataFrame()
-        test['image_id'] = list(os.listdir(TEST_DIR))
+    TEST_DIR = opt.test_dir
+    test = pd.DataFrame()
+    test['image_id'] = list(os.listdir(TEST_DIR))
 
-        if model_arch=='vit_base_patch16_384':
-            testset= ICDARDataset(test, TEST_DIR, transforms=get_inference_transforms(384))
-        else: 
-            testset= ICDARDataset(test, TEST_DIR, transforms=get_inference_transforms(opt.img_size))
+    if model_arch=='vit_base_patch16_384':
+        testset= ICDARDataset(test, TEST_DIR, transforms=get_inference_transforms(384))
+    else: 
+        testset= ICDARDataset(test, TEST_DIR, transforms=get_inference_transforms(opt.img_size))
 
-        tst_loader = DataLoader(
-            testset, 
-            batch_size=opt.valid_bs,
-            num_workers=opt.num_workers,
-            shuffle=False,
-            pin_memory=False)
-        
+    tst_loader = DataLoader(
+        testset, 
+        batch_size=opt.valid_bs,
+        num_workers=opt.num_workers,
+        shuffle=False,
+        pin_memory=False)
 
-        model = Classifier(model_arch, n_classes).to(device)
+    models = load_models(weight_path, device)
+    
+    for model in models:
 
-        tst_preds = []
+        with torch.no_grad():
+            for _ in range(tta):
+                tst_preds += [inference_one_epoch(model, tst_loader, device)]
 
-        for i,weight in enumerate(weights[ix*2:ix*2+2]):    
+    avg_tst_preds = np.mean(tst_preds, axis=0)
 
-            model.load_state_dict(torch.load(os.path.join(weight_path, weight))['model'])
+    del model
+    torch.cuda.empty_cache()
 
-            with torch.no_grad():
-                for _ in range(tta):
-                    tst_preds += [inference_one_epoch(model, tst_loader, device)]
-
-        avg_tst_preds = np.mean(tst_preds, axis=0)
-
-        if not (os.path.isdir('./total_preds')): os.mkdir('./total_preds')
-        np.save('./total_preds/total_preds.npy', tst_preds)
-
-        if not (os.path.isdir('./mean_preds')): os.mkdir('./mean_preds')
-        np.save('./mean_preds/mean_preds.npy', avg_tst_preds)
-
-        del model
-        torch.cuda.empty_cache()
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
@@ -80,7 +95,9 @@ if __name__ == "__main__":
     parser.add_argument('--valid_bs', type=int, default=16, help='validation batch size')
     parser.add_argument('--num_workers', type=int, default=2)
     parser.add_argument('--work_dir', type=str, default='CLIP/fold0', help='path to save model')
-    parser.add_argument('--weight_path', type=str, help='path to stored weights')
+    parser.add_argument('--weight_path', nargs='+', type=str, help='model.pt path(s)')  # /content/ViT
+                                                                                        # /content/resnext
+                                                                                        # /content/nfnet
     parser.add_argument('--img_size', type=int, default=224, help='resize the image')
     parser.add_argument('--tta', type=int, default=2, help='number of tta')
     opt = parser.parse_args()
--- src/models/ViT.py
+++ src/models/ViT.py
@@ -8,13 +8,13 @@ class Classifier(nn.Module):
         super().__init__()
         self.model = timm.create_model(model_name, pretrained=pretrained)
 
-        if model_name == 'tf_efficientnet_b4_ns':
+        if "efficientnet" in model_name:
             n_features = self.model.classifier.in_features
             self.model.classifier = nn.Linear(n_features, n_classes)
-        elif model_name == 'resnext50_32x4d':
+        elif "resnext" in model_name:
             n_features = self.model.fc.in_features
             self.model.fc = nn.Linear(n_features, n_classes)
-        elif model_name == 'vit_base_patch16_384':
+        elif "vit" in model_name:
             n_features = self.model.head.in_features
             self.model.head = nn.Linear(n_features, n_classes)
         elif "nfnet" in model_name:
